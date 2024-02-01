2b09113c582c143a6b7a78df153d11be92d68e16
DepressedDid <diddepressed@gmail.com>
2024-02-01 19:50:37 +0200

    MouseController update

--- Assets/Scripts/KeyBoardController.cs
+++ Assets/Scripts/KeyBoardController.cs
@@ -8,14 +8,10 @@ public class KeyBoardController : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody playerRigidbody;
-
-    public Transform emptyTransform;
-    // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = gameObject.GetComponent<Rigidbody>();
     }
-    // Update is called once per frame
     void Update()
     {
         
@@ -24,12 +20,10 @@ public class KeyBoardController : MonoBehaviour
     {
         KeyBoardMoving();
     }
-
     void KeyBoardMoving()
     {
         float directionVertical = Input.GetAxis("Vertical");
         float directionHorizontal = Input.GetAxis("Horizontal");
         playerRigidbody.velocity = (new Vector3(directionHorizontal, 0f, directionVertical) * Time.fixedDeltaTime * speed);
-        emptyTransform.position = playerRigidbody.position;
     }
 }
--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -3,21 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MouseController : MonoBehaviour
 {
     public Camera mainCamera;
     private Rigidbody playerRigidbody;
-    public Transform AxisEmpty;
-    // Start is called before the first frame update
     void Start()
     {
         playerRigidbody = gameObject.GetComponent<Rigidbody>();
     }
-    // Update is called once per frame
     void Update()
     {
-        //RotationToCursor();
+       
     }
     void FixedUpdate()
     {
@@ -27,29 +25,20 @@ public class MouseController : MonoBehaviour
     {
         Vector3 cursorPosition = Input.mousePosition;
         cursorPosition.z = mainCamera.transform.position.y;
-        return AxisEmpty.InverseTransformPoint(mainCamera.ScreenToWorldPoint(cursorPosition));
+        Vector3 worldCoordinate = mainCamera.ScreenToWorldPoint(cursorPosition);
+        Vector3 localCoordinate = worldCoordinate - transform.position;
+        return localCoordinate;
     }
-
     private Vector3 ObjectToCursorVectorCalculate()
     {
         return GetCursorPositionInWorld() - Vector3.zero;
     }
-
     private void RotationToCursor()
     {
         Vector3 result = ObjectToCursorVectorCalculate();
         result.Normalize();
-        float angle = Vector3.Angle(AxisEmpty.forward, result);
-        if (ObjectToCursorVectorCalculate().x<0)
-        {
-            Quaternion deltaRotation = Quaternion.Euler(new Vector3(90f,0f,angle));
-            playerRigidbody.rotation = deltaRotation;
-        }
-        else if(ObjectToCursorVectorCalculate().x>0)
-        {
-            Quaternion deltaRotation = Quaternion.Euler(new Vector3(90f,0f,-angle));
-            playerRigidbody.rotation=deltaRotation;
-        }
-        
+        float angle = Vector3.SignedAngle(Vector3.forward, result, Vector3.up);
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(90f,0f,-angle));
+        playerRigidbody.rotation = deltaRotation;
     }
 }
